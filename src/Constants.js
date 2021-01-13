64c826fb54fee5fdaccbffcf7b9fda81159294fa
david espinal <despinal0425@gmail.com>
2021-01-12 22:08:21 -0500

    completed the code along

---
+++ src/Constants.js
@@ -0,0 +1,2 @@
+//following the rule 'seperation of use'
+export const CLIENT_URL = 'https://pokeapi.co/api/v2/pokemon/';
---
+++ src/components/Pokemon.js
@@ -0,0 +1,9 @@
+function Pokemon({name, url}) {
+  return (
+    <li className="collection-item">
+      <a href={url}>{name}</a>
+    </li>
+  )
+}
+
+export default Pokemon
